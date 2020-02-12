87d3108a67cd944014c5f7ed32ea2274e6362cda
Louis <55180044+louisroyer@users.noreply.github.com>
2020-02-12 12:55:47 +0100

    Add detection of farming mod

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local MP = minetest.get_modpath("toolranks_extras")
+local use_mtg_farming = minetest.get_modpath("farming")
 
-if minetest.settings:get_bool("toolranks_extra.farming", true) then
+if use_mtg_farming and minetest.settings:get_bool("toolranks_extra.farming", true) then
 	dofile(MP.."/hoe.lua")
 end
