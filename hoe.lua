23006c1ab91b77c352cec554fccc6f5a25ad5f9b
Louis <55180044+louisroyer@users.noreply.github.com>
2020-03-07 14:37:47 +0100

    Add dugnodes on hoe uses

--- hoe.lua
+++ hoe.lua
@@ -1,10 +1,23 @@
 local function add_hoe(material)
 	local name = "farming:hoe_"..material
 	local desc = ItemStack(name):get_definition().description
+	local hoe_on_use = ItemStack(name):get_definition().on_use
 	minetest.override_item(name, {
 		original_description = desc,
 		description = toolranks.create_description(desc, 0, 1),
-		after_use = toolranks.new_afteruse
+		after_use = toolranks.new_afteruse,
+		-- we also want hoes to increase dugnodes when farming soil
+		on_use = function(itemstack, user, pointed_thing, uses)
+			local wear = itemstack:get_wear()
+			local ret_itemstack = hoe_on_use(itemstack, user, pointed_thing, uses)
+			local hoe_uses = ret_itemstack and ret_itemstack:get_wear() - wear or 0
+			if (hoe_uses > 0) then -- increase dugnode if tool damaged
+				return toolranks.new_afteruse(ret_itemstack, user, nil, {wear = hoe_uses})
+			else
+				return ret_itemstack or nil
+			end
+		end
+
 	})
 end
 
